2790c46f8c65011a5a82bc703a2b5c5224323498
DarkassassinX <Fuzail9136@gmail.com>
2023-09-12 12:59:50 +0530

    first commit

---
+++ src/scenes/contacts/index.jsx
@@ -0,0 +1,15 @@
+import { Box } from "@mui/material";
+import { DataGrid, GridToolbar } from "@mui/x-data-grid";
+import { tokens } from "../../theme";
+import { mockDataContacts } from "../../data/mockData";
+import Header from "../../components/Header";
+import { useTheme } from "@mui/material";
+
+const Contacts = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+
+  
+};
+
+export default Contacts;
